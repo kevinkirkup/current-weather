2065854fa71abcc0b058d314f2e32be2ccd09c5a
Kevin Kirkup <kevin.s.kirkup@me.com>
2016-09-29 17:25:31 -0400

    Add script to query the minutely forcast for the specified longitude/latitude

---
+++ request.swift
@@ -0,0 +1,73 @@
+#!/usr/bin/env swift
+
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+//: TODO Request current location from the location manager
+// https://developer.apple.com/reference/corelocation/cllocationmanager
+
+var latitude = String(35.7804)
+var longitude = String(-78.6382)
+
+let arguments = CommandLine.arguments
+
+if arguments.count >= 2 {
+
+  let long_lat = arguments[1].components(separatedBy: ",")
+
+  if long_lat.count == 2 {
+
+    latitude = long_lat[0]
+    longitude = long_lat[1]
+  }
+
+} else {
+  print("Invalid Longitude/Latitude, using default.")
+}
+
+// Replace with your Dark Sky API Key
+// https://darksky.net/dev/
+
+//let apiKey = "YOUR API KEY"
+// or
+// Get the Dark Sky API Key from the environment
+let environment = ProcessInfo.processInfo.environment
+guard let apiKey = environment["DARKSKY_API_KEY"] else {
+    print("Dark Sky API Key not specified")
+    exit(1)
+}
+
+guard let url = URL(string:
+"https://api.darksky.net/forecast/\(apiKey)/\(latitude),\(longitude)?exclude=currently,hourly,daily,flags") else {
+    print("Error creating request URL")
+    exit(1)
+}
+
+let semaphore = DispatchSemaphore(value: 0)
+
+// Create a URL Request & handle the response data
+var request = URLRequest(url: url)
+request.httpMethod = "GET"
+let session = URLSession.shared
+let task = session.dataTask(with: request, completionHandler: { (returnData, response, error) in
+
+    var strData = NSString(data: returnData!, encoding: String.Encoding.utf8.rawValue)
+
+    do {
+
+        let jsonObject = try JSONSerialization.jsonObject(with: returnData!, options: .mutableLeaves)
+        print("\(jsonObject)")
+
+    } catch {
+      print("Weather Data Not Available.")
+    }
+
+    semaphore.signal()
+
+})
+
+// Send the request and wait for a response
+task.resume()
+_ = semaphore.wait(timeout: .distantFuture)
+
